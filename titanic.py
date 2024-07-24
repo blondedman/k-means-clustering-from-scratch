65f029926f90405f22204e35b833a0cf10388aac
Shankhadeep Maiti <maitishankhadeep123@gmail.com>
2024-07-24 16:19:36 +0530

    Debug titanic.py

--- titanic.py
+++ titanic.py
@@ -8,8 +8,42 @@ import numpy as np
 style.use('ggplot')
 
 df = pd.read_excel('titanic.xls')
-# print(df.head())
+print(df.head())
 
 df.drop(['body','name'], axis=1, inplace=True)
 df = df.apply(pd.to_numeric, errors='coerce').fillna(0).astype(float)
+print(df.head())
+                
+# can use something called "one-hot" encoding
+# this will split the categorical feature into multiple features of all the different options 
+
+# for a male entry, the features will be:
+# sex_male = 1, sex_female = 0
+
+# for a female entry, the features will be:
+# sex_male = 0, sex_female = 1
+
+def handling(df):
+    columns = df.columns.values
+    
+    for column in columns:
+        
+        text_digit_vals = {}
+        
+        def convert_to_int(val):
+            return text_digit_vals [val]
+        
+        if df [column].dtype != np.int64 and df [column].dtype != np. float64: 
+            column_contents = df [column].values.tolist()                
+            unique_elements = set (column_contents)
+            x = 0
+                
+            for unique in unique_elements:
+                if unique not in text_digit_vals:
+                    text_digit_vals[unique] = x
+                    x += 1
+            df [column] = list(map(convert_to_int, df[column]))
+    return df
+
+df = handling(df)
 print(df.head())
